fcb9a6b7d1134ccee936adb4462cf30f31cf0512
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-05-13 16:07:39 +0530

    Lecture 40

---
+++ 06 - May/Lecture 40/006LongestSubstringWithKUniqueCharacters.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+int main() {
+
+	string s = "aabacbebebe";
+	int n = s.size();
+	int k = 3;
+
+	int i=0; // to store the start of the window
+	int j=0; // to store the end of the windw
+
+	int max_len = 0; // to track the length of the longest substring
+	                 // i.e. window that contain k unique characters
+	
+	int count = 0; // to keep track of no. of unique characters in the window
+	
+	unordered_map<char, int> freqMap; // to store a  mapping  between characters 
+								      // present in a window & their frequencies
+	
+	while(j < n) {
+
+		// expand the window
+		freqMap[s[j]-'a']++;
+		if(freqMap[s[j]-'a'] == 1) count++;
+
+		// check for the violation of the window property
+
+		while(count > k) {
+
+			// shrink the window i.e. remove the ith char. from the window
+			freqMap[s[i]-'a']--;
+			if(freqMap[s[i]-'a'] == 0) count--;
+			i++;
+		}
+		
+		// check for the validity of the window, and update the answer
+		if(count == k) {
+			max_len = max(max_len, j-i+1);
+		}
+
+		// continue expansion of the window
+		j++;
+	}
+
+	cout << max_len << endl;
+
+	return 0;
+}
