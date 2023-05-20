16609fcf8f6092a24135de8891aa4bf64dd19cf5
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-05-20 16:43:06 +0530

    Lecture 42

---
+++ 06 - May/Lecture 42/003DP_CoinChange.cpp
@@ -0,0 +1,127 @@
+/*
+
+You are given an integer array coins representing coins of different denominations 
+and an integer amount representing a total amount of money.Return the fewest number 
+of coins that you need to make up that amount. 
+
+You may assume that you have an infinite number of each kind of coin.
+
+Don't forget to handle the case if that amount of money cannot be made up by any 
+combination of the coins.
+
+Example 1:
+
+	Input: coins = [1,2,5], amount = 11
+	Output: 3
+
+	Input: coins = [3, 6], amount = 13
+	Output: - 
+
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int minCoins(int n, vector<int>& c) {
+
+	// base case
+	if(n == 0) {
+		// count min. no. of coins required to reduce 0 to 0
+		return 0;
+	}
+
+	// recursive case
+
+	// count min. no. of coins required to reduce n to 0
+
+	int k = c.size();
+	int res = INT_MAX;
+
+	for(int j=0; j<=k-1; j++) {
+		if(n-c[j] >= 0) {
+			// ask your friend to find min. coins required to reduce n-c[j] to 0
+			res = min(res, minCoins(n-c[j], c));
+		}
+	}
+
+	return res == INT_MAX ? res : 1 + res;
+
+	
+}
+
+int minCoinsTopDown(int n, vector<int>& c, vector<int>& dp) {
+
+	// lookup 
+	if(dp[n] != -1) {
+		return dp[n];
+	}
+
+	// base case
+	if(n == 0) {
+		// count min. no. of coins required to reduce 0 to 0
+		return dp[n] = 0;
+	}
+
+	// recursive case
+
+	// count min. no. of coins required to reduce n to 0
+
+	int k = c.size();
+	int res = INT_MAX;
+
+	for(int j=0; j<k; j++) {
+		if(n-c[j] >= 0) {
+			// compute the min. no. of coins required to reduce n-c[j] to 0 -> friend
+			res = min(res, minCoinsTopDown(n-c[j], c, dp));
+		}
+	}
+
+	return dp[n] = res == INT_MAX ? INT_MAX : 1 + res;
+
+}
+
+// time : O(nk) space : O(n)
+
+int minCoinsBottomUp(int n, vector<int> c) {
+	vector<int> dp(n+1);
+	dp[0] = 0; // dp[0] = f(0) : base case
+
+	for(int i=1; i<=n; i++) {
+		// dp[i] = f(i) : min coins required to reduce amount i to 0
+		int k = c.size();
+		int res = INT_MAX;
+
+		for(int j=0; j<k; j++) {
+			if(i-c[j] >= 0) {
+				// compute the min. no. of coins required to reduce n-c[j] to 0 -> friend
+				res = min(res, dp[i-c[j]]);
+			}
+		}
+
+		dp[i] = res == INT_MAX ? INT_MAX : 1 + res;
+	}
+
+	return dp[n]; // dp[n] = f(n) : original problem
+}
+
+
+int main() {
+
+	int n = 3;
+	vector<int> c = {1, 2};
+
+	minCoins(n, c) == INT_MAX ? cout << "no solution exists" << endl :
+	                            cout << minCoins(n, c) << endl; ;
+
+	vector<int> dp(n+1, -1);
+	cout << minCoinsTopDown(n, c, dp) << endl;
+
+	int res = minCoinsBottomUp(n, c);
+
+	res == INT_MAX ? cout << "no solution exists" << endl :
+	                 cout << res << endl;
+
+	return 0;
+}
